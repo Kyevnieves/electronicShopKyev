5fda3c4aeb1f3d02dc418b36524dad22848a199f
Kyevnieves <111917540+Kyevnieves@users.noreply.github.com>
2023-03-07 10:05:25 -0400

    tailwindConfig

---
+++ js/tailwind/tailwindConfig.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend:{
+            colors:{
+                primary: 'f0b700',
+                secondary: 'of2755',
+                background: 'f3efef',
+                stroke: '66a2c7',
+                blue: '66a2c7'
+            }
+        }
+    }
+}
